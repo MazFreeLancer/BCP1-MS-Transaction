0df0ca900d9cf2a140bc7e646f7225a06cbb1bfc
MZena <manuelzenareque@gmail.com>
2022-03-19 14:32:22 -0500

    add transaction create logic

--- src/main/java/com/nttdata/MSTransaction/service/TransactionServiceImpl.java
+++ src/main/java/com/nttdata/MSTransaction/service/TransactionServiceImpl.java
@@ -1,7 +1,11 @@
 package com.nttdata.MSTransaction.service;
 
+import com.nttdata.MSTransaction.MsTransactionApplication;
+import com.nttdata.MSTransaction.model.Account;
 import com.nttdata.MSTransaction.model.Transaction;
 import com.nttdata.MSTransaction.repository.TransactionRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -10,13 +14,48 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
+    private static final Logger logger = LogManager.getLogger(MsTransactionApplication.class);
 
     @Autowired
     TransactionRepository transactionRepository;
 
+    @Autowired
+    AccountServiceImpl accountService;
+
     @Override
     public Mono<Transaction> createTransaction(Transaction t) {
-        return transactionRepository.save(t);
+        logger.info("Ingresa al create transaction");
+        Mono<Account> accountMono = accountService.getAccountById(t.getIdAccount());
+
+        logger.info("Trabajamos con el account");
+        return accountMono.flatMap(account -> {
+            Float balanceAux = account.getBalance();
+            Float balanceFinal = Float.valueOf(0);
+            logger.info("Asignamos balance: " + balanceAux);
+            if(t.getTransactionType().toString().equals("DEPOSIT")){
+                logger.info("Trabajamos con deposito");
+                balanceFinal = balanceAux + t.getAmount();
+                logger.info("balanceFinal: " + balanceFinal);
+                account.setBalance(balanceFinal);
+                logger.info("accountTransaction: " + account.toString());
+                accountService.updateAccount(account);
+                return transactionRepository.save(t);
+            }else if(t.getTransactionType().toString().equals("CASHOUT")){
+                logger.info("Trabajamos con retiro");
+                if(balanceAux < t.getAmount()){
+                    return Mono.error(new
+                            Exception("Insufficient fund"));
+                }else{
+                    account.setBalance(balanceAux - t.getAmount());
+                    accountService.updateAccount(account);
+                    return transactionRepository.save(t);
+                }
+            }
+            return Mono.error(new
+                    Exception("Something happened"));
+        });
+
+        //return transactionRepository.save(t);
     }
 
     @Override
